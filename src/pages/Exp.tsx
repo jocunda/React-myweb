7fdf47a66815576a6ea8d62248b0365eb59d8792
jocunda <silviawanshan88@gmail.com>
2021-07-15 11:00:49 +0800

    commit -exp

--- src/pages/Exp.tsx
+++ src/pages/Exp.tsx
@@ -1,7 +1,6 @@
 import './Page.css'
 import styled from "styled-components"
-import { FaBriefcase, FaGraduationCap, GrLanguage } from 'react-icons/all'
-import { useRef } from 'react';
+import { FaBriefcase, FaGraduationCap, MdLanguage } from 'react-icons/all'
 
 const DataWork = [
     { company: "https://picsum.photos/id/1035/1900/600", key: 0 },
@@ -11,28 +10,29 @@ const DataWork = [
 ]
 
 interface LangProps {
-    color: string
+    color?: string
 }
 
 export const LanguageP = styled.p<LangProps>`
-    color:${props => props.color}
+    &:hover {
+        color:${props => props.color}
+    }
 `;
 
-
 export default function Exp() {
-    const inputEl = useRef()
+
     return <>
         <FaBriefcase />
         <FaGraduationCap />
-        <GrLanguage />
-        Language
+
         <div className="language-icon">
-            <LanguageP ref={inputEl} color={'red'} title="English" content={ } />
-            <p title="Chinese">中</p>
-            <p title="Indonesia">Id</p>
-            <p title="Cantonese">粵</p>
-            <p title="Teochew">潮</p>
-            <p title="Hakka">客</p>
+            <p><MdLanguage className="lang-icon" /> Language</p>
+            <LanguageP color={"rgb(237, 45, 45)"} title="English">En</LanguageP>
+            <LanguageP color={"rgb(255, 101, 33)"} title="Chinese" >中</LanguageP>
+            <LanguageP color={"rgb(254, 246, 55)"} title="Indonesia">Id</LanguageP>
+            <LanguageP color={"rgb(127, 251, 3)"} title="Cantonese" >粵</LanguageP>
+            <LanguageP color={"rgb(14, 181, 247)"} title="Teochew" >潮</LanguageP>
+            <LanguageP color={"rgb(188, 13, 255)"} title="Hakka" >客</LanguageP>
         </div>
         <div>Experience</div>
     </>
